f9bf35608838852b74c37fbf49cbdb9a071ff3de
NBBrathen <Noel@brathen.net>
2025-07-26 20:05:09 +0200

    Setup Django Rest Framework API

--- kalooli/settings.py
+++ kalooli/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig',
+    'api.apps.AppConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [

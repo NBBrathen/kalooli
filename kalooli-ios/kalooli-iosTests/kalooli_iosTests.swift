0cfe787f3931f4d5a71deaf81ac59a9fc218c3da
NBBrathen <Noel@brathen.net>
2025-07-28 22:57:58 -0400

    feat: Added kalooli-ios file

---
+++ kalooli-ios/kalooli-iosTests/kalooli_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  kalooli_iosTests.swift
+//  kalooli-iosTests
+//
+//  Created by Noel Brathen on 7/28/25.
+//
+
+import Testing
+@testable import kalooli_ios
+
+struct kalooli_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ kalooli-ios/kalooli-iosUITests/kalooli_iosUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  kalooli_iosUITestsLaunchTests.swift
+//  kalooli-iosUITests
+//
+//  Created by Noel Brathen on 7/28/25.
+//
+
+import XCTest
+
+final class kalooli_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
